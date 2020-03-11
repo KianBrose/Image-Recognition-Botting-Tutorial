053463edf04bad7b9c37d664537380212c63e2ba
Kian Brose <62078964+KianBrose@users.noreply.github.com>
2020-03-11 22:51:57 +0100

    Delete screenshot.py

--- screenshot.py
+++
@@ -1,6 +0,0 @@
-#This script saves the image of the region 660,350,600,400 as savedimage.png in the path "C:\Users\Antec\Desktop\Tutorial\savedimage.png"
-
-import pyautogui
-
-im1 = pyautogui.screenshot(region=(660,350,600,400))
-im1.save(r"C:\Users\Antec\Desktop\Tutorial\savedimage.png")
