50e905e326794d66b7802af6148141037e46fce6
Kian Brose <62078964+KianBrose@users.noreply.github.com>
2021-07-30 03:52:07 +0200

    Updated delay in click function
    Delay in click function changed from 0.01 to 0.1 seconds due to the click not working for a lot of people on the 0.01 delay

--- pianotiles.py
+++ pianotiles.py
@@ -13,7 +13,7 @@ import win32api, win32con
 def click(x,y):
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    time.sleep(0.01) #This pauses the script for 0.01 seconds
+    time.sleep(0.1) #This pauses the script for 0.1 seconds
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
 while keyboard.is_pressed('q') == False:
